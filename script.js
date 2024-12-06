b82a9cf3999bc67f8612dfc5fd7d7c818aa71755
Durga-velu <durgadevi134babu@gmail.com>
2024-12-06 21:52:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 //your JS code here. If required.
 function daysOfYear(year) {
-	if ((year%4===0 && year%100!==0) || (year%400===0)) {
-		alert parseInt("366");
+	if (parseInt(year%4===0 && year%100!==0) ||parseInt(year%400===0)) {
+		return 366;
 	}
 	else{
-	    alert parseInt("365");
+	    return 365;
 	}
 }
 const year = prompt("year");
 year=number.parseInt(year);
-alert(daysOfYear(year) + return );
+alert(daysOfYear(year));
